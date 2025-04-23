d336ddb9b95c8a65bf6caf8e22de7f93a355a962
Ron-911 <115991984+Ron-911@users.noreply.github.com>
2025-04-23 16:57:11 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,7 +7,49 @@ from session_manager import get_stealth_context
 OUTPUT_FILE = "data/bans.csv"
 FAILED_FILE = "data/failed_countries.txt"
 
-country_links = {  # Ваш список ссылок }
+country_links = {
+    "Albania": "https://trafficban.com/country.albania.home.2.ru.html",
+    "Austria": "https://trafficban.com/country.austria.home.14.ru.html",
+    "Belarus": "https://trafficban.com/country.belarus.home.25.ru.html",
+    "Belgium": "https://trafficban.com/country.belgium.home.20.ru.html",
+    "Bosnia and Herzegovina": "https://trafficban.com/country.bosnia_and_herzegovina.home.27.ru.html",
+    "Bulgaria": "https://trafficban.com/country.bulgaria.home.32.ru.html",
+    "Croatia": "https://trafficban.com/country.croatia.home.37.ru.html",
+    "Czech Republic": "https://trafficban.com/country.czech_republic.home.41.ru.html",
+    "Denmark": "https://trafficban.com/country.denmark.home.42.ru.html",
+    "Estonia": "https://trafficban.com/country.estonia.home.50.ru.html",
+    "Finland": "https://trafficban.com/country.finland.home.55.ru.html",
+    "France": "https://trafficban.com/country.france.home.56.ru.html",
+    "Germany": "https://trafficban.com/country.germany.home.135.ru.html",
+    "Greece": "https://trafficban.com/country.greece.home.61.ru.html",
+    "Hungary": "https://trafficban.com/country.hungary.home.212.ru.html",
+    "Ireland": "https://trafficban.com/country.ireland.home.81.ru.html",
+    "Italy": "https://trafficban.com/country.italy.home.213.ru.html",
+    "Kazakhstan": "https://trafficban.com/country.kazakhstan.home.95.ru.html",
+    "Latvia": "https://trafficban.com/country.latvia.home.110.ru.html",
+    "Liechtenstein": "https://trafficban.com/country.liechtenstein.home.219.ru.html",
+    "Lithuania": "https://trafficban.com/country.lithuania.home.108.ru.html",
+    "Luxembourg": "https://trafficban.com/country.luxembourg.home.109.ru.html",
+    "Macedonia": "https://trafficban.com/country.macedonia.home.111.ru.html",
+    "Moldova": "https://trafficban.com/country.moldova.home.126.ru.html",
+    "Montenegro": "https://trafficban.com/country.montenegro.home.40.ru.html",
+    "Netherlands": "https://trafficban.com/country.netherlands.home.76.ru.html",
+    "Norway": "https://trafficban.com/country.norway.home.140.ru.html",
+    "Poland": "https://trafficban.com/country.poland.home.151.ru.html",
+    "Portugal": "https://trafficban.com/country.portugal.home.153.ru.html",
+    "Romania": "https://trafficban.com/country.romania.home.157.ru.html",
+    "Russia": "https://trafficban.com/country.russia.home.52.ru.html",
+    "Serbia": "https://trafficban.com/country.serbia.home.168.ru.html",
+    "Slovakia": "https://trafficban.com/country.slovakia.home.172.ru.html",
+    "Slovenia": "https://trafficban.com/country.slovenia.home.173.ru.html",
+    "Spain": "https://trafficban.com/country.spain.home.75.ru.html",
+    "Sweden": "https://trafficban.com/country.sweden.home.182.ru.html",
+    "Switzerland": "https://trafficban.com/country.switzerland.home.181.ru.html",
+    "Turkey": "https://trafficban.com/country.turkey.home.193.ru.html",
+    "Ukraine": "https://trafficban.com/country.ukraine.home.198.ru.html",
+    "United Kingdom": "https://trafficban.com/country.united_kingdom.home.205.ru.html",
+}
+
 
 async def scrape():
     async with async_playwright() as p:
